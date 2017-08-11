0747eea8b4f149f6bfb3354f2fab5d29a49568ad
17liamnaddell <liamnprg@gmail.com>
2017-08-11 17:07:20 -0400

    finish this branch for now

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//	key := genKey()
-	//	printRpay(key)
+	key := genKey()
+	printRpay(key)
 
 }
 
 //print map of Rune Array
-//func printRpay(dag map[rune]rune) {
-//	for k, v := range dag {
-//		fmt.Printf("\"%s\":\"%s\"\n", string(k), string(v))
-//	}
-//}
+func printRpay(dag map[rune]rune) {
+	for k, v := range dag {
+		fmt.Printf("\"%s\":\"%s\"\n", string(k), string(v))
+	}
+}
